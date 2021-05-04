8eb5f5bb2152ae6b597d3103e083b4f594140206
baudu <bastien.audu@gmail.com>
2021-05-04 11:14:17 +0200

    ggez

--- class/Tranche.h
+++ class/Tranche.h
@@ -5,6 +5,11 @@
 #ifndef PROJET_TRANCHE_H
 #define PROJET_TRANCHE_H
 
+#include <vector>
+#include "Carton.h"
+
+using namespace std;
+
 
 class Tranche {
     Tranche(int largeur, int longueur, int hauteur);
@@ -12,11 +17,13 @@ class Tranche {
     int getLargeur() { return _largeur; }
     int getLongueur() { return _longueur; }
     int getHauteur() { return _hauteur; }
+    vector<Carton> getLesCartons() { return _lesCartons;}
 
 private:
     int _largeur;
     int _longueur;
     int _hauteur;
+    vector<Carton> _lesCartons;
 };
 
 #endif //PROJET_TRANCHE_H
