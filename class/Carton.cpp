d570ec852608fabedc053732b3b94cc987872ae3
baudu <bastien.audu@gmail.com>
2021-05-04 10:36:13 +0200

    Tri des cartons + affichage des volumes

--- class/Carton.cpp
+++ class/Carton.cpp
@@ -8,3 +8,4 @@ Carton::Carton(int largeur, int longueur, int hauteur)
         : _largeur(largeur), _longueur(longueur), _hauteur(hauteur) {
     this->_volume = ((double)longueur/100)*((double)largeur/100)*((double)hauteur/100);
 };
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <algorithm>
 #include "class/Lecture.h"
 
 int main() {
     Camion *unCamion = Lecture::getCamion();
     std::vector<Carton> lesCartons = Lecture::getCarton();
+
+    //Tri des cartons par leur volume
+    struct comparVolume
+    {
+        inline bool operator() (Carton struct1, Carton struct2)
+        {
+            return (struct1.getVolume() > struct2.getVolume());
+        }
+    };
+    std::sort(lesCartons.begin(), lesCartons.end(), comparVolume());
+
+    //Affichage des volumes des cartons
+    for (auto & lesCarton : lesCartons)
+        std::cout << lesCarton.getVolume() << ' ';
+
+
     return 0;
 }
